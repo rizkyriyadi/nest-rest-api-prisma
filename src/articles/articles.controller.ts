1b55e9c3cc61a17baea19c5c282789fa39bb3dad
yadi <rizkyriyadi16@gmail.com>
2024-07-09 05:54:39 +0700

    init project

---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ArticleEntity } from './entities/article.entity';
+
+@Controller('articles')
+@ApiTags('Articles')
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Post()
+  create(@Body() createArticleDto: CreateArticleDto) {
+    return this.articlesService.create(createArticleDto);
+  }
+
+  @Get()
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get('draft')
+  draft() {
+    return this.articlesService.draft();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
+    return this.articlesService.update(+id, updateArticleDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.articlesService.remove(+id);
+  }
+}
