45c9c819174208990eadcd01ea68224e3241a435
yadi <rizkyriyadi16@gmail.com>
2024-07-09 12:07:52 +0700

    feat(pipe) enable whitelist on pipe

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
@@ -35,17 +36,20 @@ export class ArticlesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: string) {
     return this.articlesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateArticleDto: UpdateArticleDto,
+  ) {
     return this.articlesService.update(+id, updateArticleDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: string) {
     return this.articlesService.remove(+id);
   }
 }
