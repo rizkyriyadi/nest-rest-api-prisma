6f3fb721e39ff2c2f9ec5fe654e1601e6a6d22c1
yadi <rizkyriyadi16@gmail.com>
2024-08-01 21:04:03 +0700

    rollback port to 3000

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(2113);
+  await app.listen(3000);
 }
 bootstrap();
