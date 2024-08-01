93b91c403fb260765e272ea5977176baba63ee17
yadi <rizkyriyadi16@gmail.com>
2024-08-01 20:49:37 +0700

    feat:change port to 443

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -4,6 +4,12 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 export class LoggerMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     console.log(`Request... ${req} Response... ${res}`);
+    if (res.statusCode === 200 || res.statusCode === 201) {
+      console.log(`Suscessful Request... `);
+    } else {
+      throw new Error(`Error Request... `);
+    }
+
     next();
   }
 }
