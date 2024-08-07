1ebb890a34d1681a1efd932b0ce18271f79f4e78
rizkyriyadi <87072818+rizkyriyadi@users.noreply.github.com>
2024-08-07 19:04:52 +0700

    feat{oauth} setup basic login google oauth 2.0

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,14 @@
+// src/auth/auth.module.ts
+
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { GoogleStrategy } from './google.strategy';
 
 @Module({
-  providers: [AuthService]
+  imports: [PassportModule],
+  providers: [AuthService, GoogleStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
