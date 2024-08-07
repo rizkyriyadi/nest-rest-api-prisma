fbafe88bd3d7b1efd56de04dae470ec3fc212794
rizkyriyadi <87072818+rizkyriyadi@users.noreply.github.com>
2024-08-07 23:06:06 +0700

    setup basic front end

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Response } from 'express'; // Add this line
+import { Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -9,5 +10,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('signup')
+  getSignupPage(@Res() res: Response): void {
+    res.sendFile(join(__dirname, '..', 'public', 'signup.html'));
+  }
+
+  @Get('login')
+  getLoginPage(@Res() res: Response): void {
+    res.sendFile(join(__dirname, '..', 'public', 'login.html'));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { register } from 'prom-client';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // Metrics
