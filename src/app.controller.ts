94ab97a339e4d639229da793ec9efdb86e675a35
rizkyriyadi <87072818+rizkyriyadi@users.noreply.github.com>
2024-08-08 20:47:53 +0700

    fix bug on app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { join } from 'path';
+import { join, resolve } from 'path';
 import { Response } from 'express';
 
 @Controller()
 export class AppController {
   @Get('signup')
   getSignupPage(@Res() res: Response): void {
-    res.sendFile(join(__dirname, '..', 'public', 'signup.html'));
+    const filePath = resolve('dist/public/signup.html');
+    console.log('Serving file from:', filePath);
+    res.sendFile(filePath);
   }
 
   @Get('login')
   getLoginPage(@Res() res: Response): void {
-    res.sendFile(join(__dirname, '..', 'public', 'login.html'));
+    const filePath = resolve('dist/public/login.html');
+    console.log('Serving file from:', filePath);
+    res.sendFile(filePath);
   }
 }
